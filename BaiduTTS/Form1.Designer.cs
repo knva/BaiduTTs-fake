04986c984f388ed06127c5521a514ed872c54251
knva <txt_ch@163.com>
2018-07-24 17:49:36 +0800

    https://github.com/knva/BaiduTTs-fake.git

--- BaiduTTS/Form1.Designer.cs
+++ BaiduTTS/Form1.Designer.cs
@@ -28,13 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Font = new System.Drawing.Font("宋体", 32F);
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(343, 56);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("宋体", 32F);
+            this.button1.Location = new System.Drawing.Point(362, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "♫";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_ClickAsync);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(476, 81);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.MaximumSize = new System.Drawing.Size(492, 120);
+            this.MinimumSize = new System.Drawing.Size(492, 120);
+            this.Name = "Form1";
+            this.Text = "BaiduTTS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- BaiduTTS/Form1.cs
+++ BaiduTTS/Form1.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.Collections;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using NAudio.Wave;
 
 namespace BaiduTTS
 {
@@ -16,5 +15,58 @@ namespace BaiduTTS
         {
             InitializeComponent();
         }
+
+        private async void button1_ClickAsync(object sender, EventArgs e)
+        {
+          
+            Hashtable table = new Hashtable();
+            table.Add("lan", "zh");
+            table.Add("text", textBox1.Text);
+            table.Add("spd", 5);
+            table.Add("source", "web");
+            await Task.Run(()=> {
+                string ttsurl = makeUrl("http://fanyi.baidu.com/gettts", table);
+                using (var mf = new MediaFoundationReader(ttsurl))
+                using (var wo = new WaveOutEvent())
+                {
+                    wo.Init(mf);
+                    wo.Play();
+                    while (wo.PlaybackState == PlaybackState.Playing)
+                    {
+                        Thread.Sleep(1000);
+                    }
+                }
+
+            });
+        
+
+
+        }
+
+        public static string makeUrl(string url,Hashtable dic) {
+            if (string.IsNullOrEmpty(url))
+            {
+                throw new ArgumentNullException("url");
+            }
+
+            StringBuilder builder = new StringBuilder();
+            builder.Append(url);
+            if (dic != null && dic.Count > 0)
+            {
+                builder.Append("?");
+                int i = 0;
+                foreach (DictionaryEntry item in dic)
+                {
+                    if (i > 0)
+                        builder.Append("&");
+                    builder.AppendFormat("{0}={1}", item.Key, item.Value);
+                    i++;
+                }
+            }
+            return builder.ToString();
+
+        }
     }
+
+  
 }
